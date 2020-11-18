e3282d4db027b3533ad2d733bd3dda953f9874f7
Tim Gibney <mctimoth@users.noreply.github.com>
2020-11-18 11:04:20 -0500

    first commit for FESD-Week1

---
+++ index.js
@@ -0,0 +1,22 @@
+var itemPrice = 0.99;
+var amountOfMoneyInWallet = 10.00;
+var numberOfFriends = 57;
+var ageInYears = 54;
+var firstName = "Tim";
+var lastName = "Gibney";
+var middleInitial = "P";
+var ItemPrice = 0.99;
+var AmountOfMoneyInWallet = 10.00;
+var NumberOfFriends = 57;
+var AgeInYears = 54;
+var FirstName = "Tim";
+var LastName = "Gibney";
+var MiddleInitial = "P";
+var newAmountOfMoneyInWallet = amountOfMoneyInWallet - ItemPrice;
+var numberOfFriendsMadeEachYear = numberOfFriends/AgeInYears;
+var fullName = firstName + " " + MiddleInitial + ". " + lastName;
+
+console.log("Amount of money in wallet after purchase = " + newAmountOfMoneyInWallet)
+console.log("Number of friends made each year:  " + numberOfFriendsMadeEachYear)
+console.log("MY full name is:  " + fullName);
+
